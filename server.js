aa57af42bb395a4a52067a690ebe0bb8710e6a30
Carlos Vega <carlos.vega@detektor.local>
2017-12-18 07:53:49 -0600

    Prueba 3

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 var server = http.createServer();
 function control(petic, resp) {
 resp.writeHead(200, {'content-type': 'text/plain'});
-resp.write('Hola, Mundo!');
+resp.write('Hola, Mundo 2!');
 resp.end();
 }
 server.on('request', control);
-server.listen(8080);
+server.listen(8088);
